7000c18616c2afa3f71ab4ee759dcc1616ab4239
T SURAJ KUMAR <110660073+kumarsuraj14253@users.noreply.github.com>
2023-10-31 13:17:58 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,15 +1,26 @@
-#!/bin/bash
-
-sudo apt update -y
-
-sudo apt upgrade -y 
-
-sudo apt install openjdk-17-jre -y
-
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y 
-sudo apt-get install jenkins -y
+- Install Jenkins on the EC2 instance to automate deployment:
+    Install Java
+    
+    ```bash
+    sudo apt update
+    sudo apt install fontconfig openjdk-17-jre
+    java -version
+    openjdk version "17.0.8" 2023-07-18
+    OpenJDK Runtime Environment (build 17.0.8+7-Debian-1deb12u1)
+    OpenJDK 64-Bit Server VM (build 17.0.8+7-Debian-1deb12u1, mixed mode, sharing)
+    
+    #jenkins
+    sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+    https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+    echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install jenkins
+    sudo systemctl start jenkins
+    sudo systemctl enable jenkins
+    ```
+    
+    - Access Jenkins in a web browser using the public IP of your EC2 instance.
+        
+        publicIp:8080
